6f34e5f3a438f463f9bd0de163c75ad9016d4a10
VetMen <snekzleberg2@gmail.com>
2025-01-02 11:47:15 +0700

    feat: signin & signup firebase

---
+++ config.py
@@ -0,0 +1,10 @@
+firebase_config = {
+  "apiKey": "AIzaSyDWVhMXlAWiqGZm6EMSBhdduIX0DdLXxSs",
+  "authDomain": "flask-15084.firebaseapp.com",
+  "projectId": "flask-15084",
+  "storageBucket": "flask-15084.firebasestorage.app",
+  "messagingSenderId": "232870956793",
+  "appId": "1:232870956793:web:fa5f1d3600de8271499c28",
+  "measurementId": "G-CLS4T1PDQ4",
+  "databaseURL": ""
+}
